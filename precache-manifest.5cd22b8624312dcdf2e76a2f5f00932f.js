1fa3f2bf13fba1a5c50aa014d0334406c7217b40
Blazeey <rey6venkatesh@gmail.com>
2023-08-03 21:51:35 -0400

    Updates

---
+++ precache-manifest.5cd22b8624312dcdf2e76a2f5f00932f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b98cb3624cce5d0e7096af888307cc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b850add7e5dd4ad09b6",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "9db13eb19b87f7ae8d35",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "78e986621348dcc2d325",
+    "url": "/static/css/main.edf4b3f5.chunk.css"
+  },
+  {
+    "revision": "f4f875811c7a477c50ec",
+    "url": "/static/js/2.044d38ff.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.044d38ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b850add7e5dd4ad09b6",
+    "url": "/static/js/3.7f9e9abc.chunk.js"
+  },
+  {
+    "revision": "9db13eb19b87f7ae8d35",
+    "url": "/static/js/4.54379bfe.chunk.js"
+  },
+  {
+    "revision": "78e986621348dcc2d325",
+    "url": "/static/js/main.666ffebd.chunk.js"
+  },
+  {
+    "revision": "ece502f711a477e30ead",
+    "url": "/static/js/runtime-main.c1bfc078.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "97bee7339c1ea4e8672327141e654508",
+    "url": "/static/media/accessium.97bee733.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "257895b2b43e299d646cb396369f06c4",
+    "url": "/static/media/lti.257895b2.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "d1eadbaab3ed2b74de943ae1bfcccf9a",
+    "url": "/static/media/ub.d1eadbaa.jpeg"
+  }
+]);
